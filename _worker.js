de5f59936018fcfffb051709395714e2b83ac8b8
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-03-24 19:31:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -20,7 +20,7 @@ let addressescsv = [
 ];
 
 let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/Mazeorz/airports/master/Clash/Examine_Full.ini"; //订阅配置文件
 let addressesnotls = [];
 let addressesnotlsapi = [
 	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt',
